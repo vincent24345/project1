a5f3eacae12fd9af15b2c03ae310915d799aea91
Vincent Nguyen <vincentn1102@gmail.com>
2024-12-15 17:32:20 +1030

    tutorial video python

---
+++ testing.py
@@ -0,0 +1,26 @@
+#calculator 
+
+#addition function
+def add(a,b):
+    return a+b
+#subtraction funciton
+def sub(a,b):
+    return a-b
+#multiplication function
+def mult(a,b):
+    return a *b
+#division function
+def div(a,b):
+    if b == 0 :
+        return "Error. Cannot be done"
+    return a /b
+
+#calculator 
+def main():
+    print("Welcome this is my calculator")
+    print("The available operations:")
+    print("addition : "+"")
+    print("subtraction : "-"")
+    print("division : "/"")
+    print("multiplication : "*"")
+    print("type exit to leave the calculator")
